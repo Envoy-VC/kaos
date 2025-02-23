235b60a429abcbeb04a9c0a0d078eda231ed8441
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-24 03:58:14 +0530

    chore: add basic eliza agent

---
+++ packages/kaos-agent/src/cache/index.ts
@@ -0,0 +1,14 @@
+import {
+  CacheManager,
+  type Character,
+  DbCacheAdapter,
+  type IDatabaseCacheAdapter,
+} from '@elizaos/core';
+
+export function initializeDbCache(
+  character: Character,
+  db: IDatabaseCacheAdapter
+) {
+  const cache = new CacheManager(new DbCacheAdapter(db, character.id));
+  return cache;
+}
