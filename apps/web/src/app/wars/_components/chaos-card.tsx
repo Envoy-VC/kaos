5c9d50260f8dd72d06ee9dcf4a9edf3695eee726
Vedant Chainani <vedantchainani1084@gmail.com>
2025-02-25 07:21:58 +0530

    chore: make card responsive for mobile

--- apps/web/src/app/wars/_components/chaos-card.tsx
+++ apps/web/src/app/wars/_components/chaos-card.tsx
@@ -10,28 +10,28 @@ interface ChaosCardProps {
 
 export const ChaosCard = () => {
   return (
-    <div className='flex h-[12rem] w-[32rem] flex-row gap-2 rounded-lg border-[3px] border-black bg-[#B2E7B9] px-4 py-3 font-comic'>
+    <div className='flex h-full min-w-[20rem] flex-col gap-2 rounded-lg border-[3px] border-black bg-[#B2E7B9] px-4 py-3 font-comic sm:h-[12rem] sm:min-w-[30rem] sm:flex-row'>
       <div className='flex h-full w-full basis-2/3 flex-col justify-between gap-2 text-wrap'>
         <div className='font-semibold text-2xl'>
           Is Star Wars more iconic than Star Trek?
         </div>
         <div className='font-bold text-neutral-700'>⏳ Collapse in: 2h 15m</div>
-        <div className='flex flex-row items-center gap-2'>
+        <div className='flex flex-row items-center justify-between gap-2 sm:justify-start'>
           <Button
-            className='!rounded-xl !text-lg h-10 border-2 border-black'
+            className='!rounded-xl !text-lg h-10 w-full border-2 border-black'
             variant='secondary'
           >
             Fork Reality
           </Button>
           <Button
-            className='!rounded-xl !text-lg h-10 border-2 border-black'
+            className='!rounded-xl !text-lg h-10 w-full border-2 border-black'
             variant='destructive'
           >
             Burn it Down
           </Button>
         </div>
       </div>
-      <div className='flex w-full basis-1/3 flex-col items-end justify-between'>
+      <div className='flex w-full basis-1/3 flex-row items-end justify-start gap-2 sm:flex-col sm:justify-between'>
         <div className='flex w-fit flex-row items-center justify-center gap-2 rounded-2xl border-2 border-black bg-white px-2 py-[2px]'>
           <div className='flex flex-row items-center gap-1'>
             1.2M <CircleArrowUp className='rotate-45 text-green-500' />
--- apps/web/src/app/wars/page.tsx
+++ apps/web/src/app/wars/page.tsx
@@ -2,7 +2,11 @@ import { ChaosCard } from './_components';
 
 const Wars = () => {
   return (
-    <div className='mx-auto my-[10rem] max-w-screen-xl'>
+    <div className='mx-auto my-[6dvh] flex max-w-screen-2xl flex-row flex-wrap items-center justify-center gap-4 px-2'>
+      <ChaosCard />
+      <ChaosCard />
+      <ChaosCard />
+      <ChaosCard />
       <ChaosCard />
     </div>
   );
